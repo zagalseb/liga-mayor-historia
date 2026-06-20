7fca6ccd7bc3d6bf1f03a6f865dc1fde3f395526
zagalseb <zagalseb@gmail.com>
2026-06-19 20:37:04 -0600

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://zagalseb.github.io',
+  base: '/liga-mayor-historia',
+});
